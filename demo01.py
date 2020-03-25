d53ced4502e5cbf9ffb03661abb03701c6caf769
zhaochuanjin <1330714434@qq.com>
2020-03-25 17:47:14 +0800

    提交代码

---
+++ demo01.py
@@ -0,0 +1,52 @@
+'''
+print("你好宝贝~") #  字符串
+print(345)  # 整数
+print(2.635)  # 小数
+print(True)  #  布尔值
+print(())   #   元组
+"""
+赵传金
+"""
+print("哈哈"+"嘿嘿")  # 字符串的并接
+print((1+2)*(8-3)/4)
+print(2>3)
+input()
+'''
+
+
+# a = int(input("请输入:"))
+# b = int(input("请输入:"))
+# print("input获取的内容:",a+b)
+
+
+a = int(len(input("请输入:")))
+b = int(len(input("请输入:")))
+print("字符:",a+b)
+
+
+
+
+
+
+
+
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+
+
+
+
+
+
+
+
+'''
+print(2,"这")
+'''
+
